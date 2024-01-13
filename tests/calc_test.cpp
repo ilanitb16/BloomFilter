99579f020bcfd27ae52a21464041f00124a4150d
ori2236 <orile03@gmail.com>
2024-01-13 18:01:29 +0200

    add texts

---
+++ tests/calc_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include "C:\Users\ORI LEVI\Desktop\university\second year\first semester\adv prog\facebook-iso/src/calc.cpp" // here we include the code to be tested
+TEST(SumTest, BasicTest) {
+EXPECT_EQ(sum(1,2), 3);
+}
