c5ba5e057d7bc72ce40ad2a894576288e6691868
Ori Levi <orile03@gmail.com>
2024-04-29 12:21:52 +0300

    communicate with the Node.js server

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,34 +3,34 @@
 #include "HashFunctions.h"
 #include "InputOutput.h"
 #include <iostream>
+#include <string>
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-/*
-int main()
-{
-    InputOutput inputOutput;
-    std::string setting = inputOutput.setSetting();
-    int arraySize = inputOutput.firstNum(setting);
-    HashFunctions hashFunctions(setting);
-    BitsArray bitsArray(arraySize);   
-
-    bitsArray.commandAndURL(inputOutput, hashFunctions);
-}
-*/
-
-
 using namespace std;
 
+std::string cutUrl(const char* buffer) {
+    // Find the position of the first occurrence of ".com" in the buffer
+    const char* com_pos = strstr(buffer, ".com");
+    if (com_pos != nullptr) {
+        // Calculate the length of the URL, including ".com"
+        size_t url_len = com_pos - buffer + 4; // Include the length of ".com"
+        // Extract the URL from the buffer
+        return std::string(buffer, url_len);
+    } else {
+        return ""; // ".com" not found in the buffer
+    }
+}
+
 bool isSet = false;
-HashFunctions hashFunctions;
-BitsArray bitsArray;
+HashFunctions hashFunctions("1 1");
+BitsArray bitsArray(0);
 InputOutput inputOutput;
 
 int main() {
-    const int server_port = 5555;
+    const int server_port = 7979;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("error creating socket");
@@ -53,45 +53,56 @@ int main() {
         return 1;
     }
 
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock, (struct sockaddr *)&client_sin, &addr_len);
-    if (client_sock < 0) {
-        perror("error accepting client");
-        return 1;
-    }
-
-    char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        // connection is closed
-    } else if (read_bytes < 0) {
-        // error
-    } else {
-        //if the bit array isn't set
-        if (isSet) {
-            bool bloomFilterResult = bitsArray.commandAndURL(inputOutput, hashFunctions, buffer);
-            if(bloomFilterResult) {
-                buffer[0] = 1;
-            } else {
-                buffer[0] = 0;
-            }
-        } else {
-            isSet = true;
-            string setting = buffer;
-            int arraySize = inputOutput.firstNum(setting);
-            HashFunctions newHashFunctions(setting);
-            hashFunctions = newHashFunctions;
-            BitsArray empty(arraySize);
-            bitsArray = empty;
+    while (true) {
+        struct sockaddr_in client_sin;
+        unsigned int addr_len = sizeof(client_sin);
+        int client_sock = accept(sock, (struct sockaddr *)&client_sin, &addr_len);
+        if (client_sock < 0) {
+            perror("error accepting client");
+            return 1;
         }
-    }
-    
-    int sent_bytes = send(client_sock, buffer, read_bytes, 0);
-    if (sent_bytes < 0) {
-        perror("error sending to client");
-        return 1;
+
+        char buffer[4096];
+        int expected_data_len = sizeof(buffer);
+        int read_bytes;
+
+        while ((read_bytes = recv(client_sock, buffer, expected_data_len, 0)) > 0) {
+            cout << "connection working: " << buffer << endl;
+
+            //if the bit array isn't set
+            if (isSet) {
+                string numAndUrl = cutUrl(buffer);
+                cout << "number and url: " << numAndUrl << endl;
+                bool bloomFilterResult = bitsArray.commandAndURL(inputOutput, hashFunctions, numAndUrl);
+                if (bloomFilterResult) {
+                    buffer[0] = 1;
+                } else {
+                    buffer[0] = 0;
+                }
+            } else {
+                isSet = true;
+                string setting = buffer;
+                int arraySize = inputOutput.firstNum(setting);
+                HashFunctions newHashFunctions(setting);
+                hashFunctions = newHashFunctions;
+                BitsArray empty(arraySize);
+                bitsArray = empty;
+            }
+
+            int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+            if (sent_bytes < 0) {
+                perror("error sending to client");
+                return 1;
+            }
+        }
+
+        if (read_bytes == 0) {
+            
+        } else {
+            perror("error receiving from client");
+        }
+
+        close(client_sock);
     }
 
     close(sock);
