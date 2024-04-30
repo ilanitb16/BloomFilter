4b65102a3070d36f468cd41f754336a10756f69e
Ori Levi <orile03@gmail.com>
2024-04-30 14:04:47 +0300

    multi-threaded server

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <thread>
 
 using namespace std;
 
@@ -28,6 +29,50 @@ bool isSet = false;
 HashFunctions hashFunctions("1 1");
 BitsArray bitsArray(0);
 InputOutput inputOutput;
+
+void handleConnection(int client_sock) {
+    char buffer[4096];
+    int expected_data_len = sizeof(buffer);
+    int read_bytes;
+
+    while ((read_bytes = recv(client_sock, buffer, expected_data_len, 0)) > 0) {
+        cout << "connection working: " << buffer << endl;
+
+        //if the bit array isn't set
+        if (isSet) {
+            string numAndUrl = cutUrl(buffer);
+            cout << "number and url: " << numAndUrl << endl;
+            bool bloomFilterResult = bitsArray.commandAndURL(inputOutput, hashFunctions, numAndUrl);
+            if (bloomFilterResult) {
+                buffer[0] = 1;
+            } else {
+                buffer[0] = 0;
+            }
+        } else {
+            isSet = true;
+            string setting = buffer;
+            int arraySize = inputOutput.firstNum(setting);
+            HashFunctions newHashFunctions(setting);
+            hashFunctions = newHashFunctions;
+            BitsArray empty(arraySize);
+            bitsArray = empty;
+        }
+
+        int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+        if (sent_bytes < 0) {
+            perror("error sending to client");
+            return;
+        }
+    }
+
+    if (read_bytes == 0) {
+        // Connection closed by client
+    } else {
+        perror("error receiving from client");
+    }
+
+    close(client_sock);
+}
 
 int main() {
     const int server_port = 7979;
@@ -62,47 +107,8 @@ int main() {
             return 1;
         }
 
-        char buffer[4096];
-        int expected_data_len = sizeof(buffer);
-        int read_bytes;
-
-        while ((read_bytes = recv(client_sock, buffer, expected_data_len, 0)) > 0) {
-            cout << "connection working: " << buffer << endl;
-
-            //if the bit array isn't set
-            if (isSet) {
-                string numAndUrl = cutUrl(buffer);
-                cout << "number and url: " << numAndUrl << endl;
-                bool bloomFilterResult = bitsArray.commandAndURL(inputOutput, hashFunctions, numAndUrl);
-                if (bloomFilterResult) {
-                    buffer[0] = 1;
-                } else {
-                    buffer[0] = 0;
-                }
-            } else {
-                isSet = true;
-                string setting = buffer;
-                int arraySize = inputOutput.firstNum(setting);
-                HashFunctions newHashFunctions(setting);
-                hashFunctions = newHashFunctions;
-                BitsArray empty(arraySize);
-                bitsArray = empty;
-            }
-
-            int sent_bytes = send(client_sock, buffer, read_bytes, 0);
-            if (sent_bytes < 0) {
-                perror("error sending to client");
-                return 1;
-            }
-        }
-
-        if (read_bytes == 0) {
-            
-        } else {
-            perror("error receiving from client");
-        }
-
-        close(client_sock);
+        thread t(handleConnection, client_sock);
+        t.detach();
     }
 
     close(sock);
